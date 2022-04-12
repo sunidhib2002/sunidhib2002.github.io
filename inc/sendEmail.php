668fd489d0b105b2301eb410a2e08cbc95b36b77
Sunidhi Bansal <agilist.sunidhi@gmail.com>
2022-04-12 09:02:22 +0530

    changed message for mailer

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -50,7 +50,7 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+      else { echo "SMTP Mailer yet to be configured"; }
 		
 	} # end if - no validation error
 
